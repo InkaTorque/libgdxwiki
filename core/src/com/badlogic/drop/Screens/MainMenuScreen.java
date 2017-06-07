ee1186217fe5b20c77febf2019b050aef1ec1fab
Francisco Mora <you@example.com>
2017-06-07 18:32:59 -0500

    game + screen

---
+++ core/src/com/badlogic/drop/Screens/MainMenuScreen.java
@@ -0,0 +1,76 @@
+package com.badlogic.drop.Screens;
+
+import com.badlogic.drop.Drop;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+/**
+ * Created by Leap-Pancho on 6/7/2017.
+ */
+
+public class MainMenuScreen implements Screen{
+
+    final Drop game;
+
+    OrthographicCamera camera;
+
+    public MainMenuScreen(final Drop game)
+    {
+        this.game = game;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false,800,480);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Welcome to DROP ", 100 ,150);
+        game.font.draw(game.batch, "Tap to Begin ", 100 ,100);
+        game.batch.end();
+
+        if(Gdx.input.isTouched())
+        {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
