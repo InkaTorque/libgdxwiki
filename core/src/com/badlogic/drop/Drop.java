723280a1b92fb9160234ab6837e40ba1644f8bfe
Francisco Mora <you@example.com>
2017-06-07 16:33:30 -0500

    first commit

---
+++ core/src/com/badlogic/drop/Drop.java
@@ -0,0 +1,100 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
+
+public class Drop extends ApplicationAdapter {
+
+	private Texture dropImgae;
+	private Texture bucketImage;
+	private Sound dropSound;
+	private Music rainMusic;
+
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+
+	private Rectangle bucket;
+
+    private Array<Rectangle> raindrops;
+    private long lastDropTime;
+
+
+
+	@Override
+	public void create () {
+
+		dropImgae = new Texture(Gdx.files.internal("droplet.png"));
+		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+		rainMusic.setLooping(true);
+		rainMusic.play();
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false,800,400);
+
+        batch = new SpriteBatch();
+
+		bucket = new Rectangle();
+		bucket.x = 800 / 2 - 64/2;
+		bucket.y = 20;
+		bucket.width = 64;
+		bucket.height = 64;
+
+        raindrops = new Array<Rectangle>();
+        spawnRaindrop();
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0,0,0.2f,1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(bucketImage,bucket.x,bucket.y);
+		batch.end();
+
+		if(Gdx.input.isTouched())
+		{
+			Vector3 touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(),0);
+			camera.unproject(touchPos);
+			bucket.x = touchPos.x - 64/2;
+		}
+		if(bucket.x < 0) bucket.x = 0;
+		if(bucket.x > 800 - 64) bucket.x = 800 - 64;
+
+
+		camera.update();
+
+	}
+	
+	@Override
+	public void dispose () {
+	}
+
+
+	private void spawnRaindrop(){
+        Rectangle raindrop = new Rectangle();
+        raindrop.x = MathUtils.random(0,800-64);
+        raindrop.y = 480;
+        raindrop.width = 64;
+        raindrop.height = 64;
+        raindrops.add(raindrop);
+        lastDropTime = TimeUtils.nanoTime();
+    }
+}
